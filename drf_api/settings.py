4c30b4095eac45536bc6ec1edd2a670b5589c47f
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-01-03 17:54:31 +0000

    updating CORS_ALLOWED_ORIGIN_REGEXES value in the settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -128,6 +128,7 @@ if 'CLIENT_ORIGIN' in os.environ:
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
          r"^https://.*\.gitpod\.io$",
+         r"^https://.*\.app.github\.dev$",
      ]
 
 CORS_ALLOW_CREDENTIALS = True
