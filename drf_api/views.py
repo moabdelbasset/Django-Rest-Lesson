b9c21514d9fb7209b02c4f6a1069e925ce062a51
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-01-09 09:07:49 +0000

    fixing the views.py for users login

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,14 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKE, JWT_AUTH_SAMESITE, JWT_AUTH_SECURE
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
 )
 
 
 @api_view()
 def root_route(request):
     return Response({
-        "message": "Welcome to my drf application"
+        "message": "Welcome to my drf API!"
     })
 
 
